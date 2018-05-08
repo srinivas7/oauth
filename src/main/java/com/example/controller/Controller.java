1287edde4e0bb8831256bffe42d0bfb3fc0b07a7
srbaviri <srbaviri@DIN67000624.corp.capgemini.com>
2018-05-08 15:24:24 +0530

    changed service name

--- src/main/java/com/example/controller/Controller.java
+++ src/main/java/com/example/controller/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-  @RequestMapping("/user")
+  @RequestMapping("/test")
   public String test() {
     return "test data";
   }
